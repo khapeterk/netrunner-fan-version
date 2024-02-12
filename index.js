d70815c4a9bc63d85561e20a829bd1db9e17228c
khapeterk <kha.peter.k@gmail.com>
2024-02-12 10:56:30 -0500

    draw canvas background

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const context = canvas.getContext('2d')
 canvas.width = 1280
 canvas.height = 720
 
+function drawBackground() {
+    context.fillStyle = "black"
+    context.fillRect(0, 0, canvas.width, canvas.height)
+}
+
 let count = 0;
 
 function animate() {
     window.requestAnimationFrame(animate)
+
+    drawBackground()
 
     console.log(count)
     count++
