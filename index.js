9a59ca854d4a8d0531bc2becb8b3e6c5b3a4bdcf
khapeterk <kha.peter.k@gmail.com>
2024-02-14 00:04:45 -0500

    mouse position example

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d')
 
+let mousePosition
+
 canvas.width = 1280
 canvas.height = 720
 
@@ -9,19 +11,30 @@ function drawBackground() {
     context.fillRect(0, 0, canvas.width, canvas.height)
 }
 
-let count = 0;
+function onCanvasMouseMove(event) {
+    var rect = canvas.getBoundingClientRect();
+    mousePosition = {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top,
+        toString: (event.clientX - rect.left) + ", " + (event.clientY - rect.top)
+    }
+}
+
+function write(text, x, y) {
+    context.font = "48px serif";
+    context.fillText(text, x, y);
+}
 
 const card = new Card()
-card.changePostion({x: 50, y: 100})
+card.changePostion({ x: 50, y: 100 })
 
 function animate() {
     window.requestAnimationFrame(animate)
 
     drawBackground()
     card.draw()
-    
-    console.log(count)
-    count++
+    write(mousePosition.toString, canvas.width/2, canvas.height/2)
 }
 
 animate()
+
